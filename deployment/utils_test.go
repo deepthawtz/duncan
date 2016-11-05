a2c9fdb9e834792dae6d5ed4c0bd68cce2e6c258
Dylan Clendenin <dylan@betterdoctor.com>
2016-11-04 18:33:34 -0700

    add tests

---
+++ deployment/utils_test.go
@@ -0,0 +1,50 @@
+package deployment
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestMarathonGroupID(t *testing.T) {
+	cases := []struct {
+		app string
+		env string
+	}{
+		{app: "foo", env: "stage"},
+		{app: "foo", env: "production"},
+	}
+
+	for _, test := range cases {
+		id := MarathonGroupID(test.app, test.env)
+		exp := "/" + strings.Join([]string{test.app, test.env}, "-")
+		if id != exp {
+			t.Errorf("expected %s but got %s", exp, id)
+		}
+	}
+}
+
+func TestGithubDiffLink(t *testing.T) {
+	cases := []struct {
+		app  string
+		prev string
+		tag  string
+	}{
+		{app: "foo", prev: "v1.2.3", tag: "v1.2.4"},
+		{app: "foo", prev: "v1.2.3", tag: "v1.2.3"},
+		{app: "foo", prev: "v1.2.4", tag: "v1.2.3"},
+	}
+
+	for _, test := range cases {
+		d := GithubDiffLink(test.app, test.prev, test.tag)
+		if test.prev == test.tag {
+			if d != "no changes" {
+				t.Errorf("expected no changes got %s", d)
+			}
+		}
+		// TODO: override viper YAML config to do app -> repo lookup
+		// dl := fmt.Sprintf("https://github.com/betterdoctor/%s/compare/%s...%s", test.app, test.prev, test.tag)
+		// if d != dl {
+		// 	t.Errorf("expected %s but got %s", dl, d)
+		// }
+	}
+}
