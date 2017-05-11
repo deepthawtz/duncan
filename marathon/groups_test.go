cafda93799f801bc2f88a3f4573cfd141984d90f
Dylan Clendenin <dylan@betterdoctor.com>
2017-05-11 11:56:44 -0700

    add test for marathon.GroupDefinition

--- marathon/groups_test.go
+++ marathon/groups_test.go
@@ -24,6 +24,61 @@ type testApp struct {
 	InstanceType string
 }
 
+func TestGroupDefinition(t *testing.T) {
+	cases := []struct {
+		app           string
+		env           string
+		tag           string
+		instanceTypes []string
+		ok            bool
+	}{
+		{app: "foo", env: "production", tag: "1.2.3", instanceTypes: []string{"web"}, ok: true},
+		{app: "bar", env: "stage", tag: "4.5.6", instanceTypes: []string{"web", "worker"}, ok: true},
+		{app: "bar", env: "stage", tag: "4.5.6", instanceTypes: []string{"web", "worker"}, ok: false},
+	}
+	for _, test := range cases {
+		tg := &testGroup{
+			App: test.app,
+			Env: test.env,
+			Tag: test.tag,
+		}
+		for _, it := range test.instanceTypes {
+			tg.Apps = append(tg.Apps, &testApp{InstanceType: it})
+		}
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !test.ok {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			f := filepath.Join("testdata", "group.json.tmpl")
+			b, err := ioutil.ReadFile(f)
+			if err != nil {
+				panic(err)
+			}
+			j := new(bytes.Buffer)
+			template := template.Must(template.New("task_json").Parse(string(b)))
+			err = template.Execute(j, tg)
+			if err != nil {
+				panic(err)
+			}
+			io.WriteString(w, j.String())
+		}))
+		viper.Set("marathon_host", ts.URL)
+		g, err := GroupDefinition(test.app, test.env)
+
+		if test.ok && err != nil {
+			t.Errorf("expected error to be nil but got %v", err)
+		}
+		if !test.ok && err == nil {
+			t.Errorf("expected error but got nil")
+		}
+		if test.ok && len(tg.Apps) != len(g.Apps) {
+			t.Errorf("expected %d apps but got %d", len(tg.Apps), len(g.Apps))
+		}
+	}
+}
+
 func TestList(t *testing.T) {
 	cases := []struct {
 		app           string
@@ -33,6 +88,8 @@ func TestList(t *testing.T) {
 		ok            bool
 	}{
 		{app: "foo", env: "production", tag: "1.2.3", instanceTypes: []string{"web"}, ok: true},
+		{app: "bar", env: "stage", tag: "4.5.6", instanceTypes: []string{"web", "worker"}, ok: true},
+		{app: "bar", env: "stage", tag: "4.5.6", instanceTypes: []string{"web", "worker"}, ok: false},
 	}
 	for _, test := range cases {
 		tg := &testGroup{
