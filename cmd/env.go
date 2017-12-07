cff133c99bc05afc5a7103d0ef495d95537885b2
Dylan Clendenin <dylan@betterdoctor.com>
2017-12-07 07:21:39 -0800

    [fix] check flags for all env commands

--- cmd/env.go
+++ cmd/env.go
@@ -41,6 +41,8 @@ var (
 		Use:   "get",
 		Short: "Display ENV vars for an app",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkAppEnv(app, env)
+
 			u := consul.EnvURL(app, env)
 			env, err := consul.Read(u)
 			if err != nil {
@@ -55,6 +57,7 @@ var (
 		Use:   "set KEY=VALUE [KEY2=VALUE2 ...]",
 		Short: "Set one or more ENV var key/value pairs for an app",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkAppEnv(app, env)
 			validateKeyValues(args)
 
 			if promptModifyEnvironment("set", "env", app, env, args) {
@@ -73,6 +76,7 @@ var (
 		Use:   "del KEY [KEY ...]",
 		Short: "Delete one or more ENV vars for an app",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkAppEnv(app, env)
 			validateKeys(args)
 
 			if promptModifyEnvironment("delete", "env", app, env, args) {
